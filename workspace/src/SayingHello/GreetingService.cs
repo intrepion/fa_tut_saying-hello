9275722001b10ae51012729106f8b3f5a965970a
Oliver Forral <intrepion@gmail.com>
2026-04-09 22:41:26 -0600

    6. Green: Make The Empty-String Test Pass

--- workspace/src/SayingHello/GreetingService.cs
+++ workspace/src/SayingHello/GreetingService.cs
@@ -6,6 +6,11 @@ public sealed class GreetingService : IGreetingService
 {
     public string Greet(string name)
     {
+        if (name.Length == 0)
+        {
+            return "Hello!";
+        }
+
         var trimmedName = name.Trim();
 
         return $"Hello, {trimmedName}!";
