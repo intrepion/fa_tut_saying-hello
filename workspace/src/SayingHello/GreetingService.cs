df5cf2d6c1c749541acbf869835bd039dee0f73c
Oliver Forral <intrepion@gmail.com>
2026-04-09 22:42:53 -0600

    8. Green: Finish The Core Behavior

--- workspace/src/SayingHello/GreetingService.cs
+++ workspace/src/SayingHello/GreetingService.cs
@@ -6,12 +6,12 @@ public sealed class GreetingService : IGreetingService
 {
     public string Greet(string name)
     {
-        if (name.Length == 0)
+        var trimmedName = name.Trim();
+
+        if (trimmedName.Length == 0)
         {
             return "Hello!";
         }
-
-        var trimmedName = name.Trim();
 
         return $"Hello, {trimmedName}!";
     }
