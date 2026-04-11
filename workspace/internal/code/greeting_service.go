4a72ac0401828915471b2656274bf87b1ddf8f40
Oliver Forral <intrepion@gmail.com>
2026-04-10 20:38:03 -0600

    6. Green: Return The Generic Greeting For Empty Input

--- workspace/internal/code/greeting_service.go
+++ workspace/internal/code/greeting_service.go
@@ -6,5 +6,9 @@ type GreetingService struct{}
 
 func (s GreetingService) Greet(name string) string {
 	trimmed := strings.TrimSpace(name)
+	if trimmed == "" {
+		return "Hello!"
+	}
+
 	return "Hello, " + trimmed + "!"
 }
