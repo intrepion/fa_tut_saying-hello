c26fd3342ca50d276a78bbf04fe8b0afdd56d2b1
Oliver Forral <intrepion@gmail.com>
2026-04-12 11:19:22 -0600

    Define greeting contracts

--- workspace/lib/contracts/greeting_api.dart
+++ workspace/lib/contracts/greeting_api.dart
@@ -1,1 +1,5 @@
+import 'greeting_response.dart';
 
+abstract class GreetingApi {
+  Future<GreetingResponse> getGreeting(String name);
+}
--- workspace/lib/contracts/greeting_response.dart
+++ workspace/lib/contracts/greeting_response.dart
@@ -1,1 +1,9 @@
+class GreetingResponse {
+  final String message;
 
+  const GreetingResponse({required this.message});
+
+  factory GreetingResponse.fromJson(Map<String, dynamic> json) {
+    return GreetingResponse(message: json['message'] as String);
+  }
+}
