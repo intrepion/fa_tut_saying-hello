e3fca9014d00b94b1152f5dba255b6de0ff83713
Oliver Forral <intrepion@gmail.com>
2026-04-12 11:35:03 -0600

    2. Green: Return The Personalized Greeting

--- workspace/lib/code/load_greeting.dart
+++ workspace/lib/code/load_greeting.dart
@@ -1,1 +1,14 @@
+import '../contracts/greeting_api.dart';
 
+class GreetingViewModel {
+  final String submittedName;
+  final String message;
+
+  const GreetingViewModel({required this.submittedName, required this.message});
+}
+
+Future<GreetingViewModel> loadGreeting(String name, GreetingApi api) async {
+  final response = await api.getGreeting(name);
+
+  return GreetingViewModel(submittedName: name, message: response.message);
+}
