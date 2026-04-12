bb09fe3f56705dbb4603dcfa2f322b15552cc649
Oliver Forral <intrepion@gmail.com>
2026-04-12 11:44:25 -0600

    3. Red: Add The Greeting Page Widget Test

--- workspace/test/adapter/greeting_page_test.dart
+++ workspace/test/adapter/greeting_page_test.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:saying_hello/adapter/greeting_page.dart';
+import 'package:saying_hello/contracts/greeting_api.dart';
+import 'package:saying_hello/contracts/greeting_response.dart';
 
+class MockGreetingApi extends Mock implements GreetingApi {}
+
+void main() {
+  testWidgets('renders the greeting returned by the API', (tester) async {
+    final api = MockGreetingApi();
+    when(
+      () => api.getGreeting('Ada'),
+    ).thenAnswer((_) async => const GreetingResponse(message: 'Hello, Ada!'));
+
+    await tester.pumpWidget(MaterialApp(home: GreetingPage(api: api)));
+
+    await tester.enterText(find.byKey(const ValueKey('name-input')), 'Ada');
+    await tester.tap(find.byKey(const ValueKey('submit-button')));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Hello, Ada!'), findsOneWidget);
+  });
+}
