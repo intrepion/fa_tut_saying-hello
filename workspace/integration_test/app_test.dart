3275315c7154d3872346631be814b7b3fdf429d9
Oliver Forral <intrepion@gmail.com>
2026-04-12 11:45:22 -0600

    5. Red: Add The Integration Test

--- workspace/integration_test/app_test.dart
+++ workspace/integration_test/app_test.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:saying_hello/adapter/greeting_page.dart';
+import 'package:saying_hello/contracts/greeting_api.dart';
+import 'package:saying_hello/contracts/greeting_response.dart';
 
+class MockGreetingApi extends Mock implements GreetingApi {}
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('submits the form and shows the greeting', (tester) async {
+    final api = MockGreetingApi();
+    when(
+      () => api.getGreeting('Ada'),
+    ).thenAnswer((_) async => const GreetingResponse(message: 'Hello, Ada!'));
+
+    await tester.pumpWidget(MaterialApp(home: GreetingPage(api: api)));
+    await tester.enterText(find.byKey(const ValueKey('name-input')), 'Ada');
+    await tester.tap(find.byKey(const ValueKey('submit-button')));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Hello, Ada!'), findsOneWidget);
+  });
+}
