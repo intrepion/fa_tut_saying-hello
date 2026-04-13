8e8492afc92bb78e41fcbc90ed165a225c4f6920
Oliver Forral <intrepion@gmail.com>
2026-04-12 21:19:04 -0600

    1. Red: Add The First Failing Code Test

--- workspace/test/code/default_greeting_service_test.dart
+++ workspace/test/code/default_greeting_service_test.dart
@@ -1,1 +1,12 @@
+import 'package:saying_hello/code/default_greeting_service.dart';
+import 'package:test/test.dart';
 
+void main() {
+  test('returns the personalized greeting for a non-empty name', () {
+    final service = DefaultGreetingService();
+
+    final result = service.greet('Ada');
+
+    expect(result, 'Hello, Ada!');
+  });
+}
