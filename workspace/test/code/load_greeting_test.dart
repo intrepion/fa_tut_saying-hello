878843f09269cc5ac682fc42de491f078dfc8e30
Oliver Forral <intrepion@gmail.com>
2026-04-12 23:20:11 -0600

    1. Red: Add The First Failing Code Test

--- workspace/test/code/load_greeting_test.dart
+++ workspace/test/code/load_greeting_test.dart
@@ -1,1 +1,21 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:saying_hello/code/load_greeting.dart';
+import 'package:saying_hello/contracts/greeting_api.dart';
+import 'package:saying_hello/contracts/greeting_response.dart';
+import 'package:test/test.dart';
 
+class MockGreetingApi extends Mock implements GreetingApi {}
+
+void main() {
+  test('returns the personalized greeting for a non-empty name', () async {
+    final api = MockGreetingApi();
+    when(
+      () => api.getGreeting('Ada'),
+    ).thenAnswer((_) async => const GreetingResponse(message: 'Hello, Ada!'));
+
+    final result = await loadGreeting('Ada', api);
+
+    expect(result.submittedName, 'Ada');
+    expect(result.message, 'Hello, Ada!');
+  });
+}
