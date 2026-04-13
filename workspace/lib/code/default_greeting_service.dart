66ccaf9211c03d5199a905ab100efc3a6bc9ffd5
Oliver Forral <intrepion@gmail.com>
2026-04-12 21:20:06 -0600

    2. Green: Return The Personalized Greeting

--- workspace/lib/code/default_greeting_service.dart
+++ workspace/lib/code/default_greeting_service.dart
@@ -1,1 +1,8 @@
+import '../contracts/greeting_service.dart';
 
+class DefaultGreetingService implements GreetingService {
+  @override
+  String greet(String name) {
+    return 'Hello, $name!';
+  }
+}
